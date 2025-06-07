0b4370cb5c651604bb0f06c85e3b3425d7a54f51
noitorraa <ivankonuk00@gmail.com>
2025-06-08 04:44:41 +0700

    исправил шифрование при поиске

--- MessengerServer/Services/UserService.cs
+++ MessengerServer/Services/UserService.cs
@@ -141,18 +141,19 @@ namespace MessengerServer.Services
             return new OkObjectResult(new { Message = "Аватар успешно изменён" });
         }
 
-        public async Task<ActionResult<List<User>>> SearchUsersByLogin(string login)
-        {
-            var users = await _context.Users
-                .Where(u => u.Username.Contains(login))
-                .ToListAsync();
+public async Task<ActionResult<List<User>>> SearchUsersByLogin(string login)
+{
+    var encryptedLogin = _encryptionService.EncryptDeterministic(login);
+    var users = await _context.Users
+        .Where(u => u.Username.Contains(encryptedLogin))
+        .ToListAsync();
 
-            if (users == null || !users.Any())
-            {
-                return new NotFoundObjectResult(new { Message = "Пользователи не найдены" });
-            }
-            return new OkObjectResult(users);
-        }
+    if (users == null || !users.Any())
+    {
+        return new NotFoundObjectResult(new { Message = "Пользователи не найдены" });
+    }
+    return new OkObjectResult(users);
+}
 
         public async Task<ActionResult<Chat>> GetExistingChat(int user1Id, int user2Id)
         {
